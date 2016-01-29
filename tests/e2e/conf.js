db87308aa7b58c2af7374825928fb7086f0e9166
giamir <giamir.buoncristiani@gmail.com>
2016-01-29 17:16:02 +0000

    setup environment

---
+++ tests/e2e/conf.js
@@ -0,0 +1,19 @@
+exports.config = {
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  capabilities: { 'browserName': 'chrome' },
+  specs: ['todoListFeature.js'],
+
+  jasmineNodeOpts: {
+    showColors: true
+  },
+
+  onPrepare: function() {
+    var jasmineReporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmineReporters.TerminalReporter({
+      verbosity: 3,
+      color: true,
+      showStack: true
+    }));
+  }
+};
